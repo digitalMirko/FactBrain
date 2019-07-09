ddbc46cea55cc362e91f2f6df2ef764df56a9cc4
Mirko Cukich <mirkocukich@gmail.com>
2019-07-08 22:16:36 -0500

    Initial Commit

---
+++ Fact Brain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fact Brain
+//
+//  Created by Mirko Cukich on 7/8/19.
+//  Copyright © 2019 Digital Mirko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
